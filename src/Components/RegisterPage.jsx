92a9441e4c7d5a02a38358e6e83d41d51868c770
@kumar-mohit782 <mohit.kumar@antiersolutions.com>
2022-09-10 13:18:19 +0530

    fix register

--- src/Components/RegisterPage.jsx
+++ src/Components/RegisterPage.jsx
@@ -1,43 +1,59 @@
-import React from 'react';
-import { AiOutlineClose } from 'react-icons/ai'
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import styles from './Register.module.css';
-import register from './register';
-import { useState } from 'react';
+import React from "react";
+import { AiOutlineClose } from "react-icons/ai";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import styles from "./Register.module.css";
+import register from "./register";
+import { useState } from "react";
 
-const LogIn = ({login, setLogin}) => {
+const LogIn = ({ login, setLogin }) => {
+  let [meterId, setMeterId] = useState("");
 
-   let [meterId , setMeterId] =useState("");
+  function temp() {
+    // setMeterId(e.target.value);
 
-  function temp(e){
-    setMeterId(e.target.value);
-  
     console.log(meterId);
-    console.log('this is working');
+    // console.log("this is working");
     register(meterId);
-
   }
-
-  return (  
-    <div >
-      {
-        login &&
+  const handleInputChange = (e) => {
+    setMeterId(e.target.value);
+    console.log(meterId);
+  };
+  return (
+    <div>
+      {login && (
         <div className={styles.loginContainer}>
-          <AiOutlineClose size={30} className={styles.close} onClick={() => setLogin(false)} />
+          <AiOutlineClose
+            size={30}
+            className={styles.close}
+            onClick={() => setLogin(false)}
+          />
           <Form className={styles.loginForm}>
             <h2 className={styles.heading}>Meter Id</h2>
-            <Form.Group className={styles.formGroup} controlId="formBasicEmail">
-              <Form.Control type="email" id='get' onBlur={(e)=>{temp(e)}} className={styles.formControl} placeholder="Enter Meter Number" />
+            <Form.Group className={styles.formGroup}>
+              <Form.Control
+                type="email"
+                id="get"
+                onChange={handleInputChange}
+                className={styles.formControl}
+                placeholder="Enter Meter Number"
+              />
             </Form.Group>
-            <Form.Group className={styles.formGroup} controlId="formBasicPassword">
-            <Button varient="success" className={styles.loginButton} >Register Now</Button>
-          </Form.Group>
+            <Form.Group className={styles.formGroup}>
+              <Button
+                varient="success"
+                className={styles.loginButton}
+                onClick={temp}
+              >
+                Register Now
+              </Button>
+            </Form.Group>
           </Form>
         </div>
-      }
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default LogIn;
